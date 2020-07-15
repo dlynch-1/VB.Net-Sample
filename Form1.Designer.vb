57edb382a7d81189e577b7c7156a67b2464a69ee
Daniel Lynch <dlynch_1@hotmail.com>
2020-07-15 11:12:07 -0500

    Initial Load

---
+++ Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cmbCardType = New System.Windows.Forms.ComboBox()
+        Me.cmbTransType = New System.Windows.Forms.ComboBox()
+        Me.txtRef = New System.Windows.Forms.TextBox()
+        Me.btnTest = New System.Windows.Forms.Button()
+        Me.btnSend_Lines = New System.Windows.Forms.Button()
+        Me.btnSend_Image = New System.Windows.Forms.Button()
+        Me.btnSend_Init = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Location = New System.Drawing.Point(10, 101)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(97, 37)
+        Me.btnProcess.TabIndex = 0
+        Me.btnProcess.Text = "Process"
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(113, 101)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(97, 37)
+        Me.btnExit.TabIndex = 1
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(56, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Card Type"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(7, 42)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Trans Type"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(19, 69)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(49, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Ref Num"
+        '
+        'cmbCardType
+        '
+        Me.cmbCardType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbCardType.FormattingEnabled = True
+        Me.cmbCardType.Location = New System.Drawing.Point(74, 12)
+        Me.cmbCardType.Name = "cmbCardType"
+        Me.cmbCardType.Size = New System.Drawing.Size(108, 21)
+        Me.cmbCardType.TabIndex = 6
+        '
+        'cmbTransType
+        '
+        Me.cmbTransType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbTransType.FormattingEnabled = True
+        Me.cmbTransType.Location = New System.Drawing.Point(74, 39)
+        Me.cmbTransType.Name = "cmbTransType"
+        Me.cmbTransType.Size = New System.Drawing.Size(108, 21)
+        Me.cmbTransType.TabIndex = 7
+        '
+        'txtRef
+        '
+        Me.txtRef.Location = New System.Drawing.Point(74, 66)
+        Me.txtRef.Name = "txtRef"
+        Me.txtRef.Size = New System.Drawing.Size(108, 20)
+        Me.txtRef.TabIndex = 8
+        '
+        'btnTest
+        '
+        Me.btnTest.Location = New System.Drawing.Point(256, 30)
+        Me.btnTest.Name = "btnTest"
+        Me.btnTest.Size = New System.Drawing.Size(97, 37)
+        Me.btnTest.TabIndex = 9
+        Me.btnTest.Text = "Test Reboot"
+        Me.btnTest.UseVisualStyleBackColor = True
+        '
+        'btnSend_Lines
+        '
+        Me.btnSend_Lines.Location = New System.Drawing.Point(256, 82)
+        Me.btnSend_Lines.Name = "btnSend_Lines"
+        Me.btnSend_Lines.Size = New System.Drawing.Size(97, 37)
+        Me.btnSend_Lines.TabIndex = 10
+        Me.btnSend_Lines.Text = "Send Lines"
+        Me.btnSend_Lines.UseVisualStyleBackColor = True
+        '
+        'btnSend_Image
+        '
+        Me.btnSend_Image.Location = New System.Drawing.Point(256, 138)
+        Me.btnSend_Image.Name = "btnSend_Image"
+        Me.btnSend_Image.Size = New System.Drawing.Size(97, 37)
+        Me.btnSend_Image.TabIndex = 11
+        Me.btnSend_Image.Text = "Send Image"
+        Me.btnSend_Image.UseVisualStyleBackColor = True
+        '
+        'btnSend_Init
+        '
+        Me.btnSend_Init.Location = New System.Drawing.Point(256, 199)
+        Me.btnSend_Init.Name = "btnSend_Init"
+        Me.btnSend_Init.Size = New System.Drawing.Size(97, 37)
+        Me.btnSend_Init.TabIndex = 12
+        Me.btnSend_Init.Text = "Send Init"
+        Me.btnSend_Init.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(378, 248)
+        Me.Controls.Add(Me.btnSend_Init)
+        Me.Controls.Add(Me.btnSend_Image)
+        Me.Controls.Add(Me.btnSend_Lines)
+        Me.Controls.Add(Me.btnTest)
+        Me.Controls.Add(Me.txtRef)
+        Me.Controls.Add(Me.cmbTransType)
+        Me.Controls.Add(Me.cmbCardType)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnProcess)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "PayLink VB.Net Sample"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnProcess As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents cmbCardType As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbTransType As System.Windows.Forms.ComboBox
+    Friend WithEvents txtRef As System.Windows.Forms.TextBox
+    Friend WithEvents btnTest As Button
+    Friend WithEvents btnSend_Lines As Button
+    Friend WithEvents btnSend_Image As Button
+    Friend WithEvents btnSend_Init As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,213 @@
+﻿Imports POSLink
+
+
+
+Public Class Form1
+
+    Private CARD_TYPES() As String = {"CREDIT", "DEBIT", "GIFT"}
+    Private CREDIT_TRANS_TYPES() As String = {"SALE", "AUTH", "RETURN", "POSTAUTH", "FORCEAUTH", "VOID", "ADJUST"}
+    Private DEBIT_TRANS_TYPES() As String = {"SALE", "RETURN"}
+    Private GIFT_TRANS_TYPES() As String = {"SALE", "RETURN", "RELOAD", "ACTIVATE", "DEACTIVATE", "INQUIRE"}
+    Private Sub btnTest_Click(sender As Object, e As EventArgs) Handles btnTest.Click
+        Dim posl As POSLink.PosLink = New POSLink.PosLink()
+        Dim myReboot As New ManageRequest()
+        myReboot.TransType = 13 ' reboot request
+        posl.ManageRequest = myReboot
+        Dim com1 As CommSetting = New CommSetting()
+
+        ' tcp
+        com1.CommType = "TCP"
+        com1.DestIP = "192.168.1.35"
+        com1.DestPort = "10009"
+        com1.TimeOut = "50000"
+        posl.CommSetting = com1
+
+        Dim result As ProcessTransResult = posl.ProcessTrans() '// Blocking call, will return when the transaction is complete.
+        posl.Dispose()
+    End Sub
+    Private Sub btnSend_Image_Click(sender As Object, e As EventArgs) Handles btnSend_Image.Click
+        Dim posl As POSLink.PosLink = New POSLink.PosLink()
+        Dim myImage As New ManageRequest()
+        myImage.TransType = 9 ' update Image
+        myImage.ImagePath = "C:\Temp\pics.zip"
+        posl.ManageRequest = myImage
+        Dim com1 As CommSetting = New CommSetting()
+
+        ' tcp
+        com1.CommType = "TCP"
+        com1.DestIP = "192.168.1.35"
+        com1.DestPort = "10009"
+        com1.TimeOut = "50000"
+        posl.CommSetting = com1
+
+        Dim result As ProcessTransResult = posl.ProcessTrans() '// Blocking call, will return when the transaction is complete.
+        Select Case (result.Code)
+
+            Case ProcessTransResultCode.OK
+                Dim r As ManageResponse = posl.ManageResponse
+                MessageBox.Show("ResultCode : " + r.ResultCode + "ResultTxt :" + r.ResultTxt + "ExtData :" + r.ExtData, result.Msg)
+
+            Case ProcessTransResultCode.ERROR
+                MessageBox.Show(result.Msg, "CALLED ERR")
+
+            Case Else
+                MessageBox.Show(result.Msg, "Error Processing Payment")
+
+        End Select
+
+        posl.Dispose()
+    End Sub
+    Private Sub btnProcess_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnProcess.Click
+
+
+        Dim posl As POSLink.PosLink = New POSLink.PosLink()
+
+        Dim payRequest As PaymentRequest = New PaymentRequest()  ' Setup a request object.
+
+        Dim com1 As CommSetting = New CommSetting()
+        'uart
+        '     com1.CommType = "UART";
+        '     com1.SerialPort = "COM16";
+        '     com1.TimeOut = "50000";
+        '     posl.CommSetting = com1;
+        '     //tcp
+        com1.CommType = "TCP"
+        com1.DestIP = "192.168.1.35"
+        com1.DestPort = "10009"
+        com1.TimeOut = "50000"
+        posl.CommSetting = com1
+
+        payRequest.TenderType = payRequest.ParseTenderType(cmbCardType.SelectedItem)
+        payRequest.TransType = payRequest.ParseTransType(cmbTransType.SelectedItem)
+        payRequest.ECRRefNum = txtRef.Text
+        'payRequest.Amount = "1050"   ' this will seed the transaction amount
+
+        posl.PaymentRequest = payRequest
+
+        Dim result As ProcessTransResult = posl.ProcessTrans() '// Blocking call, will return when the transaction is complete.
+
+
+
+        ' There will be 2 separate results that you must handle.  First is the ProcessTransResult, this will give you the result of the 
+        ' request to call poslink.  PaymentResponse should only be checked if ShowPageResult.Code == OK.  
+        ' PaymentResponse is the result of the financial transaction to the payment server.
+        Select (result.Code)
+
+            Case ProcessTransResultCode.OK
+                Dim r As PaymentResponse = posl.PaymentResponse
+                MessageBox.Show("ResultCode : " + r.ResultCode + "ResultTxt :" + r.ResultTxt + "ExtData :" + r.ExtData, result.Msg)
+
+            Case ProcessTransResultCode.ERROR
+                MessageBox.Show(result.Msg, "CALLED ERR")
+
+            Case Else
+                MessageBox.Show(result.Msg, "Error Processing Payment")
+
+        End Select
+
+    End Sub
+
+#Region "UI CODE"
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        cmbCardType.BeginUpdate()
+        cmbCardType.Items.Clear()
+        cmbCardType.Items.AddRange(CARD_TYPES)
+        cmbCardType.SelectedItem = CARD_TYPES(0)
+        cmbCardType.EndUpdate() ' this will trigger a changed event on this controll which is where we load the trans types.
+    End Sub
+
+    Private Sub cmbCardType_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbCardType.SelectedIndexChanged
+
+        cmbTransType.BeginUpdate()
+        cmbTransType.Items.Clear()
+
+        If (cmbCardType.SelectedIndex = 0) Then
+            cmbTransType.Items.AddRange(CREDIT_TRANS_TYPES)
+        ElseIf (cmbCardType.SelectedIndex = 1) Then
+            cmbTransType.Items.AddRange(DEBIT_TRANS_TYPES)
+        Else
+            cmbTransType.Items.AddRange(GIFT_TRANS_TYPES)
+        End If
+
+        cmbTransType.EndUpdate()
+    End Sub
+
+    Private Sub btnSend_Lines_Click(sender As Object, e As EventArgs) Handles btnSend_Lines.Click
+        Dim posl As POSLink.PosLink = New POSLink.PosLink()
+        Dim myMessage As New ManageRequest()
+        myMessage.TransType = 7 ' clear existing message
+        posl.ManageRequest = myMessage
+        Dim com1 As CommSetting = New CommSetting()
+
+        ' tcp
+        com1.CommType = "TCP"
+        com1.DestIP = "192.168.1.35"
+        com1.DestPort = "10009"
+        com1.TimeOut = "50000"
+        posl.CommSetting = com1
+
+        Dim result As ProcessTransResult = posl.ProcessTrans() '// Blocking call, will return when the transaction is complete.
+
+        myMessage.TransType = 6  ' display the message here
+        myMessage.Title = "My Title"
+        myMessage.TopDown = "N"
+        myMessage.DisplayMessage = "\CThis is line 2."
+        result = posl.ProcessTrans()
+        myMessage.DisplayMessage = "\CThis is line 1."
+        result = posl.ProcessTrans()
+        ' Wait 5 seconds then reset the terminal
+        Wait(5)
+        myMessage.TransType = 8
+        result = posl.ProcessTrans
+        posl.Dispose()
+    End Sub
+
+#End Region
+    Private Sub Wait(ByVal Seconds As Integer)
+        Dim Right_Now As Date
+
+        Right_Now = Now
+        While DateDiff(DateInterval.Second, Right_Now, Now) < Seconds
+            System.Windows.Forms.Application.DoEvents()
+        End While
+    End Sub
+
+    Private Sub btnSend_Init_Click(sender As Object, e As EventArgs) Handles btnSend_Init.Click
+        Dim posl As POSLink.PosLink = New POSLink.PosLink()
+        Dim myInit As New ManageRequest()
+        myInit.TransType = 1 ' Send INIT command
+        posl.ManageRequest = myInit
+        Dim com1 As CommSetting = New CommSetting()
+
+        ' tcp
+        com1.CommType = "TCP"
+        com1.DestIP = "192.168.1.35"
+        com1.DestPort = "10009"
+        com1.TimeOut = "50000"
+        posl.CommSetting = com1
+
+        Dim result As ProcessTransResult = posl.ProcessTrans() '// Blocking call, will return when the transaction is complete.
+        Select Case (result.Code)
+
+            Case ProcessTransResultCode.OK
+                Dim r As ManageResponse = posl.ManageResponse
+                MessageBox.Show("ResultCode : " + r.ResultCode + "ResultTxt :" + r.ResultTxt + "ExtData :" + r.ExtData, result.Msg)
+                MessageBox.Show("Terminal ID: " & r.SN & vbCrLf & "MAC Address: " & r.MacAddress, "Device Stats")
+
+            Case ProcessTransResultCode.ERROR
+                MessageBox.Show(result.Msg, "CALLED ERR")
+
+            Case Else
+                MessageBox.Show(result.Msg, "Error Processing Payment")
+
+        End Select
+
+        posl.Dispose()
+    End Sub
+End Class
